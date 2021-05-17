f8baefcee1e1045f6491fe947d24533bc0b2d84c
Ahmet Giritli <akgiritli@gmail.com>
2021-05-17 19:21:13 +0100

    water bottle and tests complete

--- src/test/java/WaterBottleTest.java
+++ src/test/java/WaterBottleTest.java
@@ -1,2 +1,38 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class WaterBottleTest {
+
+    WaterBottle waterBottle;
+
+    @Before
+    public void before(){
+        waterBottle = new WaterBottle();
+    }
+
+    @Test
+    public void hasVolume(){
+        assertEquals(100, waterBottle.getVolume());
+    }
+
+    @Test
+    public void drinkDeducts(){
+        waterBottle.drink();
+        assertEquals(90, waterBottle.getVolume());
+    }
+
+    @Test
+    public void emptiesBottle(){
+        waterBottle.empty();
+        assertEquals(0, waterBottle.getVolume());
+    }
+
+    @Test
+    public void fillsBottle(){
+        waterBottle.fill();
+        assertEquals(100, waterBottle.getVolume());
+    }
+
 }
