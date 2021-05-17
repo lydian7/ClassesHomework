f6f8bebfa1c239f8a8664830574cbaefabc1bd49
Ahmet Giritli <akgiritli@gmail.com>
2021-05-17 19:47:33 +0100

    extension and tests complete

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,2 +1,27 @@
 public class Printer {
+    private int numberOfSheets;
+    private int tonerLevel;
+
+    public Printer(){
+        this.numberOfSheets = 100;
+        this.tonerLevel = 100;
+    }
+
+    public int getNumberOfSheets(){
+        return this.numberOfSheets;
+    }
+
+    public int getTonerLevel(){
+        return this.tonerLevel;
+    }
+
+    public void print(int numberOfPages, int numberOfCopies){
+        int printTotal = numberOfPages * numberOfCopies;
+        if(printTotal <= this.numberOfSheets){
+            this.numberOfSheets -= printTotal;
+            this.tonerLevel -= printTotal;
+        }
+    }
+
+
 }
--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -1,2 +1,42 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PrinterTest {
+
+    Printer printer;
+
+    @Before
+    public void before(){
+        printer = new Printer();
+    }
+
+    @Test
+    public void getSheets(){
+        assertEquals(100, printer.getNumberOfSheets());
+    }
+
+    @Test
+    public void canPrint(){
+        printer.print(20,2);
+        assertEquals(60, printer.getNumberOfSheets());
+    }
+
+    @Test
+    public void cantPrint(){
+        printer.print(50,3);
+        assertEquals(100, printer.getNumberOfSheets());
+    }
+
+    @Test
+    public void getToner(){
+        assertEquals(100, printer.getTonerLevel());
+    }
+
+    @Test
+    public void tonerReduces(){
+        printer.print(40,2);
+        assertEquals(20, printer.getTonerLevel());
+    }
 }
