2428f4317ea8a8595aece9b289fb9e40b033168e
Ahmet Giritli <akgiritli@gmail.com>
2021-05-17 19:08:59 +0100

    first commit

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    Calculator calculator;
+
+    @Before
+    public void before(){
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void canAdd(){
+        assertEquals(4, calculator.add(2,2));
+    }
+
+    @Test
+    public void canSubtract(){
+        assertEquals(4, calculator.subtract(8,4));
+    }
+
+}
